aba798cfc2f65543525ad47618234d8b10d013f0
mehdi2010 <31897022+mehdi2010@users.noreply.github.com>
2023-01-27 20:52:46 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState, useEffect, Fragrment } from 'react'
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function App() {
+ const [text,setText]=useState()
+ const [loading,setLoading]=useState(false)
+ 
+ function loadData()
+ {
+  setTimeout(()=>{
+    setText("This is  Loading test...")
+    setLoading(false);
+  },3000)
+ }
+function HandleOnclick()
+{
+  setLoading(true)
+  loadData();
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="button" style={{margin:"5px",padding:"10px",fontSize:"14px"}} value="Start" onClick={HandleOnclick}  />
+     <h1>{ loading?(
+      <Backdrop
+      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={loading}
+    //  onClick={setLoading(!)}
+    >
+      <CircularProgress color="inherit" />
+    </Backdrop>):text
+      }</h1>
     </div>
   );
 }
